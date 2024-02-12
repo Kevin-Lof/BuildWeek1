0d39875238696c729bafb81297a3badd3b27d3dc
fabiogalli95 <38462365+fabiogalli95@users.noreply.github.com>
2024-02-12 16:01:57 +0100

    Prima Pagina

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const bttnProceed = document.getElementById('buttonProceed');
+const condizioni = document.getElementById('condizioni');
+const error = document.getElementById('error');
+let isFirstTime = true;
+condizioni.addEventListener('click', function(){
+    if(!condizioni.checked){
+        error.classList.remove('error');
+    }else{
+        error.classList.add('error');
+    }
+});
+bttnProceed.addEventListener('click', function(e){
+
+    e.preventDefault();
+    if(condizioni.checked){
+        window.location.href='benchmark.html';
+    }else{
+        error.classList.remove('error');
+    }
+})
