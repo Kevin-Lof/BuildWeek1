2ed4fc234edf3606bd5594dffa062138bef72ca0
GabScognamiglio <gabbo16s@gmail.com>
2024-02-14 10:55:30 +0100

    Co-authored-by: kevinloffredi <Kevin-Lof@users.noreply.github.com> Co-authored-by: Enrico Cirotto <enricocirotto@users.noreply.github.com> Co-authored-by: PavloPastushenkoo <PavloPastushenkoo@users.noreply.github.com>
    modifiche pagina 3

--- assets/js/result.js
+++ assets/js/result.js
@@ -5,3 +5,8 @@ let numRisposteSbagliate = 10 - numRisposteGiuste;
  risposteGiuste.innerText = `${numRisposteGiuste}0 %`;
  risposteSbagliate.innerText = `${numRisposteSbagliate}0 %`;
 
+let questionGiuste = document.getElementById('questionsGiuste')
+let questionSbagliate = document.getElementById('questionsSbagliate')
+
+questionGiuste.innerHTML=`${numRisposteGiuste}/10 questions`
+questionSbagliate.innerHTML=`${numRisposteSbagliate}/10 questions`
