15fda5545839aee44c643a6a7bd08bf654c6d2d3
GabScognamiglio <gabbo16s@gmail.com>
2024-02-14 12:13:13 +0100

    Co-authored-by: kevinloffredi <Kevin-Lof@users.noreply.github.com> Co-authored-by: Enrico Cirotto <enricocirotto@users.noreply.github.com> Co-authored-by: PavloPastushenkoo <PavloPastushenkoo@users.noreply.github.com>
    pagina 3

--- assets/js/result.js
+++ assets/js/result.js
@@ -2,11 +2,12 @@ const risposteGiuste = document.getElementById('risposteGiuste');
 const risposteSbagliate = document.getElementById('risposteSbagliate');
 let numRisposteGiuste = localStorage.getItem('rispostaGiusta');
 let numRisposteSbagliate = 10 - numRisposteGiuste;
- risposteGiuste.innerText = `${numRisposteGiuste}0 %`;
- risposteSbagliate.innerText = `${numRisposteSbagliate}0 %`;
+ risposteGiuste.innerHTML = `<b>${numRisposteGiuste}0%</b>`;
+ risposteSbagliate.innerHTML = `<b>${numRisposteSbagliate}0%</b>`;
 
 let questionGiuste = document.getElementById('questionsGiuste')
 let questionSbagliate = document.getElementById('questionsSbagliate')
 
 questionGiuste.innerHTML=`${numRisposteGiuste}/10 questions`
 questionSbagliate.innerHTML=`${numRisposteSbagliate}/10 questions`
+
