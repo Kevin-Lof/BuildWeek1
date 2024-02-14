4d2c788b5388b3eb589eeac6836f91ad3563655e
GabScognamiglio <gabbo16s@gmail.com>
2024-02-14 12:57:09 +0100

    pagina 3 - migliorie

--- assets/js/result.js
+++ assets/js/result.js
@@ -8,6 +8,20 @@ let numRisposteSbagliate = 10 - numRisposteGiuste;
 let questionGiuste = document.getElementById('questionsGiuste')
 let questionSbagliate = document.getElementById('questionsSbagliate')
 
+const cerchioTesto = document.getElementById('cerchio')
+
 questionGiuste.innerHTML=`${numRisposteGiuste}/10 questions`
 questionSbagliate.innerHTML=`${numRisposteSbagliate}/10 questions`
 
+
+const scrittaCerchioTesto = () => {
+    if (parseInt(numRisposteGiuste)>=6) {
+        cerchioTesto.innerHTML=`<p>Congratulations! <span class='celestino'>You passed the exam.</span></p> We'll send you the certificate in few minutes. Check your email (including promotions / spam folder)`
+    }
+
+    else {
+        cerchioTesto.innerHTML = `<p>We're sorry! <br> <span class='violetto'>You failed the exam.</span></p> Don't worry! You can try again and get your certificate!`
+    }
+}
+
+scrittaCerchioTesto();
